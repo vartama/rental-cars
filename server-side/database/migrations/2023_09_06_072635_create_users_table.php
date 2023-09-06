4b3c7b680ecb40926acd2cfcd16cf52bbf89653b
vartama <billylol9119@gmail.com>
2023-09-06 15:02:45 +0700

    feat: setup database and seed data admin

--- server-side/database/migrations/2023_09_06_072635_create_users_table.php
+++ server-side/database/migrations/2023_09_06_072635_create_users_table.php
@@ -11,10 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('id')->primary()->default(Str::uuid());
+            $table->string('username');
+            $table->string('password');
+            $table->string('email');
+            $table->string('role');
+            $table->timestamps();
         });
     }
 
@@ -23,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('users');
     }
 };
